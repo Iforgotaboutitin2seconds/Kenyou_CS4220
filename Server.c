fd6f7a11a7b6d39cde1d034076bd3f9e96278601
Kenyou Teoh <kenyouteoh@gmail.com>
2023-08-30 11:04:04 -0600

    updates it

--- Server.c
+++ Server.c
@@ -1,59 +1,64 @@
+// Server side C/C++ program to demonstrate Socket
+// programming
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
-
-int main()
+#include <unistd.h>
+#define PORT 8080
+int main(int argc, char const* argv[])
 {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1)
-    {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+	int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+	char* hello = "Hello from server";
 
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(0); // Define your port number
-    server_address.sin_addr.s_addr = INADDR_ANY;  // Bind to any available interface
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
-    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
-    {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET,
+				SO_REUSEADDR | SO_REUSEPORT, &opt,
+				sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
 
-    if (listen(server_socket, 1) == -1)
-    {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr*)&address,
+			sizeof(address))
+		< 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 3) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	if ((new_socket
+		= accept(server_fd, (struct sockaddr*)&address,
+				(socklen_t*)&addrlen))
+		< 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	valread = read(new_socket, buffer, 1024);
+	printf("%s\n", buffer);
+	send(new_socket, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
 
-    int client_socket = accept(server_socket, NULL, NULL);
-    if (client_socket == -1)
-    {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    char buffer[1024];
-    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received <= 0)
-    {
-        perror("Receive failed");
-    }
-    else
-    {
-        buffer[bytes_received] = '\0'; // Null-terminate the received data
-        printf("Received: %s\n", buffer);
-
-        // You can also send a response back to the client using send()
-    }
-
-    close(client_socket);
-    close(server_socket);
-
-    return 0;
+	// closing the connected socket
+	close(new_socket);
+	// closing the listening socket
+	shutdown(server_fd, SHUT_RDWR);
+	return 0;
 }
