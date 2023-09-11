7010a6e354da952b894b6841cda59d2d11192113
Kenyou Teoh <kenyouteoh@gmail.com>
2023-09-11 11:12:33 -0600

    update client, server

--- Server.c
+++ Server.c
@@ -1,67 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define SERVER_PORT 12345
+#define BUF_SIZE 4096
+#define QUEUE_SIZE 10
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
+int main(int argc,chat*argv[])
+{
+    int s,b,l,fd,sa,bytes,on=1;
+    char buf[BUF_SIZE];
+    struct sockaddr_in channel;
 
-    // Creating socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+    //build address structure to bind to socket
+    memset(&channel,0,sizeof(channel));
+    channel.sin_family=AF_INET;
+    channel.sin_addr.saddr=htonl(NADDR_ANY);
+    channel.sin_port=htons(SERVER_PORT);
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    //passive open, wait for connection
+    s=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s<0) fatal("socket failed");
+    setsockopt(s,SOL_SOCKET,SO_REUSEADDR,(char*) &on,sizeof(on));
 
-    // Bind
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
+    b=bind(s,(struct sockaddr *) &channel, sizeof(channel));
+    if(b<0) fatal("bind failed");
 
-    // Listen
-    if (listen(server_fd, 5) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
+    l=listen(s,QUEUE_SIZE);
+    if(l<0) fatal("listen failed");
 
-    printf("Server listening on port %d\n", PORT);
+    //socket is now set up and bound. Wait for connection and process it.
+    while (1){
+        //block for connection request
+        sa =accpet(s,0,0);
+        if(sa<0) fatal("accept failed");
 
-    while (1) {
-        // Accept a new connection
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-            perror("Accept failed");
-            exit(EXIT_FAILURE);
+        //read file name from socket
+        read(sa,buf,BUF_SIZE);
+
+        //get and return file
+        fd=open(buf,O_RDNOLY);
+        if (fd<0) fatal("open failed");
+
+        while(1){
+            bytes =read(fd,buf,BUF_SIZE);
+            if(bytes<=0) break;
+            write(sa, buf,bytes);
         }
-
-        printf("Client connected\n");
-
-        // Receive file
-        FILE *received_file = fopen("received_file.txt", "wb");
-        while (1) {
-            int bytes_read = recv(new_socket, buffer, BUFFER_SIZE, 0);
-            if (bytes_read <= 0) {
-                break;
-            }
-            fwrite(buffer, 1, bytes_read, received_file);
-        }
-        fclose(received_file);
-
-        printf("File received and saved\n");
-
-        close(new_socket);
-        printf("Client disconnected\n");
+        //close file
+        close(fd);
+        //close connection
+        close(sa);
     }
+}
 
-    return 0;
+fatal(char *string)
+{
+    printf("%s\n", string);
+    exit(1);
 }
--- client.c
+++ client.c
@@ -1,55 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define SERVER_PORT 12345
+#define BUF_SIZE 4096
 
-int main() {
-    int client_socket;
-    struct sockaddr_in server_address;
-    char buffer[BUFFER_SIZE];
+int main(int argc, char *argv)
+{
+    int c, s, bytes;
+    // buffer for incoming file
+    char buf[BUF_SIZE];
+    // info about server
+    struct hostent *h;
+    // holds IP address
+    struct sockaddr_in channel;
 
-    // Create socket
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+    if (argc != 3)
+        fatal("Usage: client server-name file-name");
+
+    // look up host's IP address
+    h = gethostbyname(arvg[1]);
+    if (!h)
+        fatal("socket");
+    memset(&channel, 0, sizeof(channel));
+    channel.sin_family = AF_INET;
+    memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
+    channel.sin_port = htons(SERVER_PORT);
+
+    c = connect(s, (struct sockaddr *)&channel, sizeof(channel));
+    if (c < 0)
+        fatal("connect failed");
+
+    // connection is now established. Send file name inlcuding 0 byte at end.
+    write(s, argv[2], strlen(argv[2]) + 1);
+
+    // go get the file and write it o standard output
+    while (1)
+    {
+        // read from socket
+        bytes = read(s, buf, BUF_SIZE);
+
+        // check for end of file
+        if (bytes <= 0)
+            exit(0);
+
+        // write to standard output
+        write(1, buf, bytes);
     }
+}
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    // Connect to the server
-    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("Connection failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Connected to server\n");
-
-    // Open the file to send
-    FILE *file_to_send = fopen("file_to_send.txt", "rb");
-    if (!file_to_send) {
-        perror("File open failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Send file
-    while (1) {
-        int bytes_read = fread(buffer, 1, BUFFER_SIZE, file_to_send);
-        if (bytes_read <= 0) {
-            break;
-        }
-        send(client_socket, buffer, bytes_read, 0);
-    }
-    fclose(file_to_send);
-
-    printf("File sent\n");
-
-    close(client_socket);
-
-    return 0;
+fatal(char *string)
+{
+    printf("%s\n", string);
+    exit(1);
 }
