f603a7700584688db2c0b594131a6978c36d4940
Kenyou Teoh <kenyouteoh@gmail.com>
2023-11-08 13:22:43 -0700

    updates

--- Project2/UDP_client/client.c
+++ Project2/UDP_client/client.c
@@ -1,130 +1,45 @@
-// client code for UDP socket programming 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#include <signal.h>
-#include <sys/time.h>
 
-#define IP_PROTOCOL 0 
-#define IP_ADDRESS "127.0.0.1" // localhost 
-#define PORT_NO 15050 
-#define NET_BUF_SIZE 32 
-#define cipherKey 'S' 
-#define sendrecvflag 0 
-#define TIMEOUT 3 // in seconds
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8888
+#define BUFFER_SIZE 1024
 
-// function to clear buffer 
-void clearBuf(char* b) 
-{ 
-	int i; 
-	for (i = 0; i < NET_BUF_SIZE; i++) 
-		b[i] = '\0'; 
-} 
-
-// function for decryption 
-char Cipher(char ch) 
-{ 
-	return ch ^ cipherKey; 
-} 
-
-// function to receive file 
-int recvFile(char* buf, int s) 
-{ 
-	int i; 
-	char ch; 
-	for (i = 0; i < s; i++) { 
-		ch = buf[i]; 
-		ch = Cipher(ch); 
-		if (ch == EOF) 
-			return 1; 
-		else
-			printf("%c", ch); 
-	} 
-	return 0; 
-}
-
-int sockfd;
-struct sockaddr_in addr_con;
-int addrlen = sizeof(addr_con);
-char net_buf[NET_BUF_SIZE];
-int base = 0;
-int next_seq_num = 0;
-int window_size = 4;
-int timeout_flag = 0;
-
-void timeout_handler(int signal) {
-    timeout_flag = 1;
-}
-
-int main() 
-{ 
-	// socket() 
-	sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL); 
-
-	if (sockfd < 0) {
-		perror("\nFile descriptor not received!!");
+int main() {
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 
-	printf("\nFile descriptor %d received\n", sockfd);
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server_addr.sin_port = htons(SERVER_PORT);
 
-	addr_con.sin_family = AF_INET; 
-	addr_con.sin_port = htons(PORT_NO); 
-	addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS); 
+	char filename[256];
+	printf("Enter filename: ");
+	fgets(filename, 256, stdin);
+	filename[strcspn(filename, "\n")] = 0; // remove newline character
 
-	FILE* fp; 
-	struct sigaction sa;
-    sa.sa_handler = timeout_handler;
-    sigaction(SIGALRM, &sa, NULL);
-
-	while (1) { 
-		printf("\nPlease enter file name to receive:\n"); 
-		scanf("%s", net_buf); 
-		sendto(sockfd, net_buf, NET_BUF_SIZE, 
-			sendrecvflag, (struct sockaddr*)&addr_con, 
-			addrlen); 
-
-		printf("\n---------Data Received---------\n"); 
-
-		while (1) { 
-			// receive 
-			clearBuf(net_buf);
-			alarm(TIMEOUT); // Set an alarm for TIMEOUT seconds
-			timeout_flag = 0;
-
-			while (!timeout_flag) {
-				if (recvfrom(sockfd, net_buf, NET_BUF_SIZE, 
-							 sendrecvflag, (struct sockaddr*)&addr_con, 
-							 &addrlen) > 0) {
-					alarm(0); // Reset the alarm upon successful receipt of data
-					if (net_buf[0] == 'E' && net_buf[1] == 'O' && net_buf[2] == 'F') {
-						goto end;
-					}
-
-					int seq_num = net_buf[0] - '0';
-					if (seq_num >= base && seq_num < base + window_size) {
-						printf("Received packet with sequence number: %d\n", seq_num);
-						if (seq_num == base) {
-							recvFile(net_buf + 1, NET_BUF_SIZE - 1);
-							base++;
-						}
-					}
-
-					int ack_packet = base;
-					sendto(sockfd, &ack_packet, sizeof(int),
-							sendrecvflag, (struct sockaddr*)&addr_con, addrlen);
-				}
-			}
-		}
+	FILE *fp = fopen(filename, "rb");
+	if (fp == NULL) {
+		perror("file open failed");
+		exit(EXIT_FAILURE);
 	}
-	
-	end:
-	printf("\n-------------------------------\n"); 
-	close(sockfd);
-	return 0; 
+
+	char buffer[BUFFER_SIZE];
+	int bytes_read;
+	while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, fp)) > 0) {
+		sendto(sock, buffer, bytes_read, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	}
+
+	fclose(fp);
+	close(sock);
+	return 0;
 }
--- Project2/UDP_server/server.c
+++ Project2/UDP_server/server.c
@@ -1,170 +1,48 @@
-// server code for UDP socket programming 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-#include <sys/select.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-#define IP_PROTOCOL 0 
-#define PORT_NO 15050 
-#define NET_BUF_SIZE 32 
-#define cipherKey 'S' 
-#define sendrecvflag 0 
-#define nofile "File Not Found!" 
-#define WINDOW_SIZE 4
-#define TIMEOUT 3 // in seconds
+#define BUF_SIZE 1024
+#define PORT 8080
 
-typedef struct {
-    int seq_num;
-    char data[NET_BUF_SIZE];
-} Packet;
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUF_SIZE];
+    int len, n;
 
-// function to clear buffer 
-void clearBuf(char* b) { 
-    int i; 
-    for (i = 0; i < NET_BUF_SIZE; i++) 
-        b[i] = '\0'; 
-} 
-
-// function to encrypt 
-char Cipher(char ch) { 
-    return ch ^ cipherKey; 
-} 
-
-// function sending file 
-int sendFile(FILE* fp, char* buf, int s) { 
-    int i, len; 
-    if (fp == NULL) { 
-        strcpy(buf, nofile); 
-        len = strlen(nofile); 
-        buf[len] = EOF; 
-        for (i = 0; i <= len; i++) 
-            buf[i] = Cipher(buf[i]); 
-        return 1; 
-    } 
-
-    char ch, ch2; 
-    for (i = 0; i < s; i++) { 
-        ch = fgetc(fp); 
-        ch2 = Cipher(ch); 
-        buf[i] = ch2; 
-        if (ch == EOF) 
-            return 1; 
-    } 
-    return 0; 
-} 
-
-int main() { 
-    int sockfd, nBytes; 
-    struct sockaddr_in addr_con; 
-    int addrlen = sizeof(addr_con); 
-    addr_con.sin_family = AF_INET; 
-    addr_con.sin_port = htons(PORT_NO); 
-    addr_con.sin_addr.s_addr = INADDR_ANY; 
-    char net_buf[NET_BUF_SIZE]; 
-    FILE* fp; 
-
-    // socket() 
-    sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL); 
-
-    if (sockfd < 0) 
-        printf("\nfile descriptor not received!!\n"); 
-    else
-        printf("\nfile descriptor %d received\n", sockfd); 
-
-    // bind() 
-    if (bind(sockfd, (struct sockaddr*)&addr_con, sizeof(addr_con)) == 0) 
-        printf("\nSuccessfully binded!\n"); 
-    else
-        printf("\nBinding Failed!\n"); 
-
-    int base = 0;
-    int next_seq_num = 0;
-    Packet window[WINDOW_SIZE];
-    int ack_received[WINDOW_SIZE];
-    
-    // Initialize variables
-    for (int i = 0; i < WINDOW_SIZE; ++i) {
-        ack_received[i] = 0;
+    // create socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    while (1) { 
-        printf("\nWaiting for file name...\n"); 
+    // set server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-        // receive file name 
-        clearBuf(net_buf); 
+    // bind socket to address
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
 
-        nBytes = recvfrom(sockfd, net_buf, 
-                        NET_BUF_SIZE, sendrecvflag, 
-                        (struct sockaddr*)&addr_con, &addrlen); 
+    // receive message from client
+    len = sizeof(client_addr);
+    n = recvfrom(sockfd, (char *)buffer, BUF_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &len);
+    buffer[n] = '\0';
+    printf("Received message from client: %s\n", buffer);
 
-        fp = fopen(net_buf, "r"); 
-        printf("\nFile Name Received: %s\n", net_buf); 
-        if (fp == NULL) 
-            printf("\nFile open failed!\n"); 
-        else
-            printf("\nFile Successfully opened!\n"); 
+    // send message back to client
+    sendto(sockfd, (const char *)buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
+    printf("Message sent back to client.\n");
 
-        while (1) { 
-            // process 
-            if (sendFile(fp, net_buf, NET_BUF_SIZE)) { 
-                Packet ack_packet;
-                ack_packet.seq_num = next_seq_num;
-                strcpy(ack_packet.data, net_buf);
-                sendto(sockfd, &ack_packet, sizeof(Packet), 
-                    sendrecvflag, 
-                    (struct sockaddr*)&addr_con, addrlen); 
-                next_seq_num++;
-                break; 
-            } 
-
-            // send 
-            Packet packet;
-            packet.seq_num = next_seq_num;
-            strcpy(packet.data, net_buf);
-            window[next_seq_num % WINDOW_SIZE] = packet;
-            next_seq_num++;
-
-            for (int i = base; i < next_seq_num; ++i) {
-                sendto(sockfd, &(window[i % WINDOW_SIZE]), sizeof(Packet), 
-                       sendrecvflag, (struct sockaddr*)&addr_con, addrlen);
-            }
-
-            // Set timeout for select() function
-            fd_set fdset;
-            FD_ZERO(&fdset);
-            FD_SET(sockfd, &fdset);
-            struct timeval timeout;
-            timeout.tv_sec = TIMEOUT;
-            timeout.tv_usec = 0;
-
-            // Wait for ACK or timeout
-            int activity = select(sockfd + 1, &fdset, NULL, NULL, &timeout);
-            if (activity < 0) {
-                perror("Select error");
-                exit(EXIT_FAILURE);
-            } else if (activity == 0) {
-                // Timeout, resend packets
-                continue;
-            } else {
-                // ACK received, update window and move base
-                Packet ack_packet;
-                recvfrom(sockfd, &ack_packet, sizeof(Packet), 
-                         sendrecvflag, (struct sockaddr*)&addr_con, &addrlen);
-                ack_received[ack_packet.seq_num % WINDOW_SIZE] = 1;
-
-                while (ack_received[base % WINDOW_SIZE]) {
-                    ack_received[base % WINDOW_SIZE] = 0;
-                    base++;
-                }
-            }
-        } 
-        if (fp != NULL) 
-            fclose(fp); 
-    } 
-    return 0; 
+    close(sockfd);
+    return 0;
 }
