96a8345fabfac193a5b77bfadaff9013e0a80390
Kenyou Teoh <kenyouteoh@gmail.com>
2023-11-08 13:58:39 -0700

    update

--- Project2/UDP_client/client.c
+++ Project2/UDP_client/client.c
@@ -1,72 +1,100 @@
-// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8000
 #define BUFFER_SIZE 1024
 #define WINDOW_SIZE 4
+#define TIMEOUT 5
 
-typedef struct {
-    int number;
-    char data[BUFFER_SIZE];
-} Packet;
+int sock;
+struct sockaddr_in server_addr;
+char buffer[BUFFER_SIZE];
+int bytes_read;
+int seq_num = 0;
+int window_base = 0;
+int window_end = WINDOW_SIZE - 1;
+bool is_timer_running = false;
+
+void start_timer() {
+	is_timer_running = true;
+	alarm(TIMEOUT);
+}
+
+void stop_timer() {
+	is_timer_running = false;
+	alarm(0);
+}
+
+void handle_timeout(int sig) {
+	printf("Timeout occurred. Resending packets.\n");
+	for (int i = window_base; i <= window_end; i++) {
+		sendto(sock, buffer + i * BUFFER_SIZE, BUFFER_SIZE, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	}
+	start_timer();
+}
+
+void send_packet(int seq_num) {
+	sendto(sock, buffer + seq_num * BUFFER_SIZE, BUFFER_SIZE, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if (seq_num == window_base && !is_timer_running) {
+		start_timer();
+	}
+}
 
 int main() {
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_addr.sin_port = htons(SERVER_PORT);
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server_addr.sin_port = htons(SERVER_PORT);
 
-    char filename[256];
-    printf("Enter filename: ");
-    fgets(filename, 256, stdin);
-    filename[strcspn(filename, "\n")] = 0; // remove newline character
+	char filename[256];
+	printf("Enter filename: ");
+	fgets(filename, 256, stdin);
+	filename[strcspn(filename, "\n")] = 0; // remove newline character
 
-    FILE *fp = fopen(filename, "rb");
-    if (fp == NULL) {
-        perror("file open failed");
-        exit(EXIT_FAILURE);
-    }
+	FILE *fp = fopen(filename, "rb");
+	if (fp == NULL) {
+		perror("file open failed");
+		exit(EXIT_FAILURE);
+	}
 
-    Packet window[WINDOW_SIZE];
-    int window_start = 0;
-    int packet_number = 0;
+	while ((bytes_read = fread(buffer, 1, BUFFER_SIZE * WINDOW_SIZE, fp)) > 0) {
+		window_base = 0;
+		window_end = WINDOW_SIZE - 1;
+		seq_num = 0;
+		while (window_base <= bytes_read / BUFFER_SIZE) {
+			for (int i = window_base; i <= window_end && i <= bytes_read / BUFFER_SIZE; i++) {
+				send_packet(i);
+			}
+			int expected_ack = window_base;
+			while (expected_ack <= window_end && expected_ack <= bytes_read / BUFFER_SIZE) {
+				struct sockaddr_in client_addr;
+				socklen_t client_addr_len = sizeof(client_addr);
+				int ack_num;
+				recvfrom(sock, &ack_num, sizeof(ack_num), MSG_DONTWAIT, (struct sockaddr*)&client_addr, &client_addr_len);
+				if (ack_num == expected_ack) {
+					expected_ack++;
+					window_base++;
+					window_end++;
+					stop_timer();
+				}
+			}
+		}
+	}
 
-    while (!feof(fp)) {
-        // Fill the window with packets
-        for (int i = 0; i < WINDOW_SIZE && !feof(fp); i++) {
-            window[i].number = packet_number++;
-            fread(window[i].data, 1, BUFFER_SIZE, fp);
-        }
-
-        // Send all packets in the window
-        for (int i = 0; i < WINDOW_SIZE; i++) {
-            sendto(sock, &window[i], sizeof(Packet), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
-        }
-
-        // Wait for acknowledgment
-        Packet ack;
-        recvfrom(sock, &ack, sizeof(Packet), 0, NULL, NULL);
-
-        // If acknowledgment for the first packet in the window is received, slide the window
-        if (ack.number == window_start) {
-            window_start++;
-        }
-    }
-
-    fclose(fp);
-    close(sock);
-    return 0;
+	fclose(fp);
+	close(sock);
+	return 0;
 }
--- Project2/UDP_server/server.c
+++ Project2/UDP_server/server.c
@@ -1,124 +1,53 @@
+// server.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <signal.h>
 
-#define BUF_SIZE 1024
-#define PORT 8000
+#define SERVER_PORT 8000
+#define BUFFER_SIZE 1024
 #define WINDOW_SIZE 4
-#define TIMEOUT 5
 
-typedef struct
-{
-    int seq_num;
-    char data[BUF_SIZE];
-} packet;
+typedef struct {
+    int number;
+    char data[BUFFER_SIZE];
+} Packet;
 
-void timeout_handler(int signum);
-
-int main()
-{
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[BUF_SIZE];
-    int len, n;
-
-    // create socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0)
-    {
+int main() {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // set server address
+    struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(SERVER_PORT);
 
-    // bind socket to address
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
+    if (bind(sock, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // receive message from client
-    len = sizeof(client_addr);
-    n = recvfrom(sockfd, (char *)buffer, BUF_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &len);
-    buffer[n] = '\0';
-    printf("Received message from client: %s\n", buffer);
+    Packet window[WINDOW_SIZE];
+    int window_start = 0;
 
-    // send message back to client using Go-Back-N protocol
-    int base = 0;
-    int next_seq_num = 0;
-    packet window[WINDOW_SIZE];
-    bool ack_received[WINDOW_SIZE] = {false};
-    while (base < strlen(buffer))
-    {
-        // send packets in window
-        for (int i = base; i < base + WINDOW_SIZE && i < strlen(buffer); i++)
-        {
-            packet p;
-            p.seq_num = i;
-            strncpy(p.data, buffer + i, BUF_SIZE);
-            sendto(sockfd, (const char *)&p, sizeof(p), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
-            printf("Sent packet with sequence number %d.\n", p.seq_num);
-            if (i == next_seq_num)
-            {
-                alarm(TIMEOUT);
-            }
-            next_seq_num++;
-        }
+    while (1) {
+        // Receive a packet
+        Packet packet;
+        recvfrom(sock, &packet, sizeof(Packet), 0, NULL, NULL);
 
-        // receive acknowledgments
-        while (true)
-        {
-            packet ack;
-            int n = recvfrom(sockfd, (char *)&ack, sizeof(ack), MSG_DONTWAIT, (struct sockaddr *)&client_addr, &len);
-            if (n < 0)
-            {
-                break;
-            }
-            printf("Received acknowledgment for sequence number %d.\n", ack.seq_num);
-            if (ack.seq_num >= base && ack.seq_num < base + WINDOW_SIZE)
-            {
-                ack_received[ack.seq_num - base] = true;
-            }
-            if (ack.seq_num == base)
-            {
-                // slide window
-                int i;
-                for (i = 0; i < WINDOW_SIZE && ack_received[i]; i++)
-                {
-                    ack_received[i] = false;
-                }
-                base += i;
-                next_seq_num = base;
-                alarm(0);
-                break;
-            }
-        }
-
-        // handle timeout
-        if (base < next_seq_num)
-        {
-            signal(SIGALRM, timeout_handler);
-            alarm(TIMEOUT);
-            pause();
+        // If the packet is the one we're expecting, slide the window and send an acknowledgment
+        if (packet.number == window_start) {
+            window_start++;
+            sendto(sock, &packet, sizeof(Packet), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
         }
     }
 
-    close(sockfd);
+    close(sock);
     return 0;
-}
-
-void timeout_handler(int signum)
-{
-    printf("Timeout occurred.\n");
 }
