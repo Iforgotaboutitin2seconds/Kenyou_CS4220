98ac64bc404bf7f22a66b86ea4b1ac47e380c9bc
Kenyou Teoh <kenyouteoh@gmail.com>
2023-11-08 13:43:40 -0700

    Update server.c

--- Project2/UDP_server/server.c
+++ Project2/UDP_server/server.c
@@ -12,12 +12,16 @@
 #define WINDOW_SIZE 4
 #define TIMEOUT 5
 
-typedef struct {
+typedef struct
+{
     int seq_num;
     char data[BUF_SIZE];
 } packet;
 
-int main() {
+void timeout_handler(int signum);
+
+int main()
+{
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
     char buffer[BUF_SIZE];
@@ -25,7 +29,8 @@ int main() {
 
     // create socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
+    if (sockfd < 0)
+    {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -37,7 +42,8 @@ int main() {
     server_addr.sin_port = htons(PORT);
 
     // bind socket to address
-    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -53,35 +59,43 @@ int main() {
     int next_seq_num = 0;
     packet window[WINDOW_SIZE];
     bool ack_received[WINDOW_SIZE] = {false};
-    while (base < strlen(buffer)) {
+    while (base < strlen(buffer))
+    {
         // send packets in window
-        for (int i = base; i < base + WINDOW_SIZE && i < strlen(buffer); i++) {
+        for (int i = base; i < base + WINDOW_SIZE && i < strlen(buffer); i++)
+        {
             packet p;
             p.seq_num = i;
             strncpy(p.data, buffer + i, BUF_SIZE);
             sendto(sockfd, (const char *)&p, sizeof(p), MSG_CONFIRM, (const struct sockaddr *)&client_addr, len);
             printf("Sent packet with sequence number %d.\n", p.seq_num);
-            if (i == next_seq_num) {
+            if (i == next_seq_num)
+            {
                 alarm(TIMEOUT);
             }
             next_seq_num++;
         }
 
         // receive acknowledgments
-        while (true) {
+        while (true)
+        {
             packet ack;
             int n = recvfrom(sockfd, (char *)&ack, sizeof(ack), MSG_DONTWAIT, (struct sockaddr *)&client_addr, &len);
-            if (n < 0) {
+            if (n < 0)
+            {
                 break;
             }
             printf("Received acknowledgment for sequence number %d.\n", ack.seq_num);
-            if (ack.seq_num >= base && ack.seq_num < base + WINDOW_SIZE) {
+            if (ack.seq_num >= base && ack.seq_num < base + WINDOW_SIZE)
+            {
                 ack_received[ack.seq_num - base] = true;
             }
-            if (ack.seq_num == base) {
+            if (ack.seq_num == base)
+            {
                 // slide window
                 int i;
-                for (i = 0; i < WINDOW_SIZE && ack_received[i]; i++) {
+                for (i = 0; i < WINDOW_SIZE && ack_received[i]; i++)
+                {
                     ack_received[i] = false;
                 }
                 base += i;
@@ -92,7 +106,8 @@ int main() {
         }
 
         // handle timeout
-        if (base < next_seq_num) {
+        if (base < next_seq_num)
+        {
             signal(SIGALRM, timeout_handler);
             alarm(TIMEOUT);
             pause();
@@ -103,6 +118,7 @@ int main() {
     return 0;
 }
 
-void timeout_handler(int signum) {
+void timeout_handler(int signum)
+{
     printf("Timeout occurred.\n");
 }
